d65b6ffb497ca88a8fc30bd43a35236380a86dbd
Danilo <danilo.silva02@escola.pr.gov.br>
2026-08-28 16:01:58 -0300

    script js

---
+++ script.js
@@ -0,0 +1,6 @@
+      let quantidadeLikes = 0;
+
+        function adicionarLike() {
+            quantidadeLikes++;
+            document.getElementById('contador').innerText = quantidadeLikes;
+        }
